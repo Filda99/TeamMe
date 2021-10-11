1d80a850afd2986d4f28058fa62e74da43e3c1d4
Filip Jahn <filip.jahn.cz@gmail.com>
2021-10-11 13:14:44 +0200

    first commit

---
+++ database/User.js
@@ -0,0 +1,17 @@
+const { application } = require('express');
+const { DataTypes } = require('sequelize');
+
+module.exports = {
+  // Model attributes are defined here
+  firstName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  lastName: {
+    type: DataTypes.STRING
+    // allowNull defaults to true
+  }
+}, {
+  // Other model options go here
+};
+
