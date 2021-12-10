497e2a2ec0ebb780da00d1bf37b00bf03f8430e6
Filip Jahn <filip.jahn.cz@gmail.com>
2021-12-10 13:08:41 +0100

    fix update User

--- public/script/reset_info.js
+++ public/script/reset_info.js
@@ -6,27 +6,27 @@ document.getElementById("def_workingDays").value = document.getElementById("hid_
 
 
 
-if(document.getElementById("hid_faculty").innerText == 1){
-    document.getElementById("def_faculty").value = "FAST";
-}
-if(document.getElementById("hid_faculty").innerText == 2){
-    document.getElementById("def_faculty").value = "FSI";
-}
-if(document.getElementById("hid_faculty").innerText == 3){
-    document.getElementById("def_faculty").value = "FEKT";
-}
-if(document.getElementById("hid_faculty").innerText == 4){
-    document.getElementById("def_faculty").value = "FA";
-}
-if(document.getElementById("hid_faculty").innerText == 5){
-    document.getElementById("def_faculty").value = "FCH";
-}
-if(document.getElementById("hid_faculty").innerText == 6){
-    document.getElementById("def_faculty").value = "FP";
-}
-if(document.getElementById("hid_faculty").innerText == 7){
-    document.getElementById("def_faculty").value = "FaVU";
-}
-if(document.getElementById("hid_faculty").innerText == 8){
-    document.getElementById("def_faculty").value = "FIT";
-}
+// if(document.getElementById("hid_faculty").innerText == 1){
+//     document.getElementById("def_faculty").value = "FAST";
+// }
+// if(document.getElementById("hid_faculty").innerText == 2){
+//     document.getElementById("def_faculty").value = "FSI";
+// }
+// if(document.getElementById("hid_faculty").innerText == 3){
+//     document.getElementById("def_faculty").value = "FEKT";
+// }
+// if(document.getElementById("hid_faculty").innerText == 4){
+//     document.getElementById("def_faculty").value = "FA";
+// }
+// if(document.getElementById("hid_faculty").innerText == 5){
+//     document.getElementById("def_faculty").value = "FCH";
+// }
+// if(document.getElementById("hid_faculty").innerText == 6){
+//     document.getElementById("def_faculty").value = "FP";
+// }
+// if(document.getElementById("hid_faculty").innerText == 7){
+//     document.getElementById("def_faculty").value = "FaVU";
+// }
+// if(document.getElementById("hid_faculty").innerText == 8){
+//     document.getElementById("def_faculty").value = "FIT";
+// }
