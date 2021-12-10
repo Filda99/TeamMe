7b5f87fabd73fab4ba34607b5fe95abc4230fc25
Filip Jahn <filip.jahn.cz@gmail.com>
2021-12-10 14:19:07 +0100

    fix

--- public/script/reset_info.js
+++ public/script/reset_info.js
@@ -1,5 +1,5 @@
 // document.getElementById("yearOfStudy").value = document.getElementById("hid_year").innerText;
-document.getElementById("communicationChannel").value = document.getElementById("hid_contact").innerText;
+// document.getElementById("communicationChannel").value = document.getElementById("hid_contact").innerText;
 document.getElementById("def_approach").value = document.getElementById("hid_approach").innerText;
 document.getElementById("def_workingHours").value = document.getElementById("hid_workingHours").innerText;
 document.getElementById("def_workingDays").value = document.getElementById("hid_workingDays").innerText;
